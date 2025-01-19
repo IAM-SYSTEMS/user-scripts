131189e1d2fea20e3f06e8aca90cd131f9abaf43
Josh K <120184314+JoshKelleway@users.noreply.github.com>
2025-01-19 17:34:17 +1100

    Update rclone_backup_script-HAVM.sh

--- rclone_backup_script-HAVM.sh
+++ rclone_backup_script-HAVM.sh
@@ -3,6 +3,6 @@
 #Mount HomeAssistant backup drive
 mkdir /mnt/remotes/HOMEASSISTANT_backup
 sudo mount.cifs //HOMEASSISTANT/backup /mnt/remotes/HOMEASSISTANT_backup -o username=admin,password=
-rclone sync "/mnt/remotes/HOMEASSISTANT_backup/" onedrive:havm/backups/[SERVER_NAME]
-rclone sync "/mnt/user/backups/" onedrive:unraid/backups/[SERVER_NAME]/apps
+rclone sync "/mnt/remotes/HOMEASSISTANT_backup/" onedrive:backups/[SERVER_NAME]/havm
+rclone sync "/mnt/user/backups/" onedrive:backups/[SERVER_NAME]/apps
 fusermount -u /mnt/remotes/HOMEASSISTANT_backup

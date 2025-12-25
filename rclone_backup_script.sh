0d48da2f68d0f8cf6b5b89552eb70696d9e026dd
Josh K <120184314+JoshKelleway@users.noreply.github.com>
2025-12-25 12:08:21 +1100

    Refactor rclone backup script for USB and HA backups
    Updated the rclone backup script to delete older USB backups

--- rclone_backup_script.sh
+++ rclone_backup_script.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #----------------------------------------------------------------------------
+# Find and delete all but the newest USB Backup
+DIR="/mnt/user/backups/USB/"
+ls -t $DIR | tail -n +2 | xargs -r -I {} rm -f $DIR{}
+
 ##backup app data backups
-rclone sync "/mnt/user/backups/" onedrive:backups/IAMSYS/[SERVER_NAME]/apps
+rclone sync "/mnt/user/backups/" onedrive:backups/IAMSYS/[NAME]
 
 #Mount HomeAssistant backup drive
-#mkdir /mnt/remotes/HOMEASSISTANT_backup
-#sudo mount.cifs //HOMEASSISTANT/backup /mnt/remotes/HOMEASSISTANT_backup -o username=admin,password=
+mkdir /mnt/remotes/HOMEASSISTANT_backup
+sudo mount.cifs //HOMEASSISTANT/backup /mnt/remotes/HOMEASSISTANT_backup -o username=homeassistant,password=theumbrellacorp
 
 #Backup HA Config from VM
-#rclone sync "/mnt/remotes/HOMEASSISTANT_backup/" onedrive:backups/IAMSYS/[SERVER_NAME]/havm
+rclone sync "/mnt/remotes/HOMEASSISTANT_backup/" onedrive:backups/IAMSYS/[NAME]/haos
 
 #Unmount HomeAssistant backup drive
-#fusermount -u /mnt/remotes/HOMEASSISTANT_backup
+fusermount -u /mnt/remotes/HOMEASSISTANT_backup

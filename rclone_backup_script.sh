625838121c9ea443b28862f5d53cceab47427b6b
Josh K <120184314+JoshKelleway@users.noreply.github.com>
2025-12-28 08:13:17 +1100

    Implement SERVERNAME Variable

--- rclone_backup_script.sh
+++ rclone_backup_script.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 #----------------------------------------------------------------------------
+USBDIR="/mnt/user/backups/USB/"
+SERVERNAME="IAM-SERVER"
+
 # Find and delete all but the newest USB Backup
-DIR="/mnt/user/backups/USB/"
-ls -t $DIR | tail -n +2 | xargs -r -I {} rm -f $DIR{}
+ls -t $USBDIR | tail -n +2 | xargs -r -I {} rm -f $DIR{}
 
 ##backup app data backups
-rclone sync "/mnt/user/backups/" onedrive:backups/IAMSYS/[NAME]
+rclone sync "/mnt/user/backups/" onedrive:backups/IAMSYS/$SERVERNAME
 
 #Mount HomeAssistant backup drive
 mkdir /mnt/remotes/HOMEASSISTANT_backup
 sudo mount.cifs //HOMEASSISTANT/backup /mnt/remotes/HOMEASSISTANT_backup -o username=homeassistant,password=theumbrellacorp
 
 #Backup HA Config from VM
-rclone sync "/mnt/remotes/HOMEASSISTANT_backup/" onedrive:backups/IAMSYS/[NAME]/haos
+rclone sync "/mnt/remotes/HOMEASSISTANT_backup/" onedrive:backups/IAMSYS/$SERVERNAME/haos
 
 #Unmount HomeAssistant backup drive
 fusermount -u /mnt/remotes/HOMEASSISTANT_backup
